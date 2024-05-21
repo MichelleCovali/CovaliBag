ed2533f94c8f3545cc6a2010728fcad07a911f89
TimofeiArefev <timofei.arefev@student.nhlstenden.com>
2024-05-21 10:53:28 +0200

    fixed a bug with the scroling up in the menu + update assets

--- CovaliBagApp/Menu.js
+++ CovaliBagApp/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,11 +6,11 @@ import {
   TouchableOpacity,
   Image,
   ScrollView,
-  Dimensions
-} from 'react-native';
+  Dimensions,
+} from "react-native";
 
-const { width, height } = Dimensions.get('window');
-const baseWidth = 375;  // Base screen width, e.g., iPhone X
+const { width, height } = Dimensions.get("window");
+const baseWidth = 375; // Base screen width, e.g., iPhone X
 const baseHeight = 667; // Base screen height
 
 function responsiveWidth(num) {
@@ -33,30 +33,40 @@ const Menu = () => {
   };
 
   return (
-    <ScrollView style={styles.scrollViewStyle}>
-      <View style={[styles.container, isDarkMode ? styles.darkMode : styles.lightMode]}>
+    <ScrollView
+      style={[
+        styles.scrollViewStyle,
+        isDarkMode ? styles.darkMode : styles.lightMode,
+      ]}
+    >
+      <View style={[styles.container]}>
         <View style={styles.header}>
           <ToggleModeButton isDarkMode={isDarkMode} onPress={toggleMode} />
         </View>
         <View style={styles.logoBox}>
           <Image
-            source={isDarkMode ? require('./assets/Logo1.png') : require('./assets/main.png')}
+            source={
+              isDarkMode
+                ? require("./assets/Logo1.png")
+                : require("./assets/main.png")
+            }
             style={styles.logo}
           />
         </View>
         <View style={styles.text}>
-          <Text style={[isDarkMode ? styles.titleSelectDark : styles.titleSelectLight]}>
+          <Text
+            style={[isDarkMode ? styles.darkMode : styles.lightMode, styles]}
+          >
             select
           </Text>
-          <Text style={[isDarkMode ? styles.titleBagsDark : styles.titleBagsLight]}>
+          <Text
+            style={[isDarkMode ? styles.titleBagsDark : styles.titleBagsLight]}
+          >
             Bags
           </Text>
         </View>
         <View style={[isDarkMode ? styles.bagBoxDark : styles.bagBoxLight]}>
-          <Image
-            source={require('./assets/bag.png')}
-            style={styles.bagImage}
-          />
+          <Image source={require("./assets/bag.png")} style={styles.bagImage} />
           <View style={styles.infoRow}>
             <View style={styles.footer}>
               <Text style={styles.limitedEdition}>limited edition</Text>
@@ -64,7 +74,7 @@ const Menu = () => {
             </View>
             <View style={styles.warningRow}>
               <Image
-                source={require('./assets/warning-icon.png')}
+                source={require("./assets/warning-icon.png")}
                 style={styles.warningIcon}
               />
             </View>
@@ -72,7 +82,11 @@ const Menu = () => {
         </View>
         <View style={styles.plusRow}>
           <Image
-            source={isDarkMode ? require('./assets/plusIconDark.png') : require('./assets/plusIconLight.png')}
+            source={
+              isDarkMode
+                ? require("./assets/plusIconDark.png")
+                : require("./assets/plusIconLight.png")
+            }
             style={styles.plusIcon}
           />
         </View>
@@ -85,7 +99,11 @@ const ToggleModeButton = ({ isDarkMode, onPress }) => {
   return (
     <TouchableOpacity style={styles.toggleButton} onPress={onPress}>
       <Image
-        source={isDarkMode ? require('./assets/darkmode.png') : require('./assets/lightmode.png')}
+        source={
+          isDarkMode
+            ? require("./assets/darkmode.png")
+            : require("./assets/lightmode.png")
+        }
         style={styles.modeIcon}
       />
     </TouchableOpacity>
@@ -98,85 +116,85 @@ const styles = StyleSheet.create({
   },
   header: {
     marginTop: responsiveHeight(60),
-    width: '100%',
-    flexDirection: 'row',
+    width: "100%",
+    flexDirection: "row",
     marginLeft: responsiveWidth(20),
   },
   toggleButton: {
     padding: responsiveWidth(20),
     borderRadius: responsiveWidth(20),
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
   },
   modeIcon: {
     width: responsiveWidth(24),
     height: responsiveHeight(24),
   },
   logoBox: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   logo: {
     marginBottom: responsiveHeight(20),
     width: responsiveWidth(120),
     height: responsiveHeight(100),
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   text: {
     marginLeft: responsiveWidth(30),
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
   titleSelectLight: {
-    color: '#000000',
+    color: "#000000",
     fontSize: responsiveFontSize(24),
   },
   titleSelectDark: {
-    color: '#E4BF7C',
+    color: "#E4BF7C",
     fontSize: responsiveFontSize(24),
   },
   titleBagsDark: {
     fontSize: responsiveFontSize(32),
-    fontWeight: 'bold',
-    color: '#E4BF7C',
+    fontWeight: "bold",
+    color: "#E4BF7C",
   },
   titleBagsLight: {
     fontSize: responsiveFontSize(32),
-    fontWeight: 'bold',
-    color: '#000000',
+    fontWeight: "bold",
+    color: "#000000",
   },
   bagBoxLight: {
     margin: responsiveWidth(30),
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
     borderRadius: 20,
   },
   bagBoxDark: {
     margin: responsiveWidth(30),
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#E4BF7C',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#E4BF7C",
     borderRadius: 20,
   },
   bagImage: {
     width: responsiveWidth(300),
     height: responsiveHeight(300),
-    resizeMode: 'contain',
+    resizeMode: "contain",
     margin: responsiveWidth(30),
   },
   infoRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
   },
   footer: {
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
     marginLeft: responsiveWidth(32),
   },
   limitedEdition: {
@@ -185,7 +203,7 @@ const styles = StyleSheet.create({
   },
   modelName: {
     fontSize: responsiveFontSize(22),
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginRight: responsiveWidth(10),
     marginBottom: responsiveHeight(10),
   },
@@ -197,10 +215,10 @@ const styles = StyleSheet.create({
     marginRight: responsiveWidth(10),
   },
   plusRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
     marginBottom: responsiveHeight(40),
   },
   plusIcon: {
@@ -208,10 +226,10 @@ const styles = StyleSheet.create({
     height: responsiveHeight(50),
   },
   lightMode: {
-    backgroundColor: '#E4BF7C',
+    backgroundColor: "#E4BF7C",
   },
   darkMode: {
-    backgroundColor: '#393939',
+    backgroundColor: "#393939",
   },
 });
 
