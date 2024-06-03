4a5846a0583f94725780e10af661d3ab89ca1d9a
JML-NHL-Stenden <jia.men.lam@student.nhlstenden.com>
2024-06-03 13:49:01 +0200

    debugging

--- CovaliBagApp/App.js
+++ CovaliBagApp/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import BlackScreen from "./BlackScreen";
-// import mihaelapp from "./mihaelapp";
-// import DarylApp from "./DarylApp";
-// import Menu from "./Menu";
+import mihaelapp from "./mihaelapp";
+import DarylApp from "./DarylApp";
+import Menu from "./Menu";
 import RogerApp from "./RogerApp/RogerApp";
-// import RogerAppTest from "./RogerApp/RogerApp-test";
-// import Menu2 from "./Menu2";
-// import LoadingAnimation from "./LoadingAnimation";
+import Menu2 from "./Menu2";
+import LoadingAnimation from "./LoadingAnimation";
 
 
 const Stack = createStackNavigator();
@@ -31,7 +30,6 @@ const App = () => {
         {/* <Stack.Screen name="mihaelapp" component={mihaelapp} /> */}
         {/* <Stack.Screen name="Menu" component={Menu} /> */}
         <Stack.Screen name="RogerApp" component={RogerApp} />
-
       </Stack.Navigator>
     </NavigationContainer>
   );
